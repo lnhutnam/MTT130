e7ef7164630f0af2515f8d8b9683f49d5ca3e039
lnhutnam <lenam.fithcmus@gmail.com>
2024-06-11 17:32:10 +0700

    update.

---
+++ Code1DProblem.m
@@ -0,0 +1,65 @@
+clc; clear; format long % set up
+
+L = 1; Ne = 20;
+T = 1; Nt = 20;
+
+UL = 0; UR = 8;
+
+[err, U, ele_coo] = One1DProblem(L, Ne, T, Nt, UL, UR);
+
+hold on
+X = [0 ele_coo 1]; 
+UU = [UL; U; UR];
+plot(X', UU, 'b-');
+syms x
+f = exp(-1) * sin(pi * x) + x;
+fplot(f, [0 1]);
+title('t=1')
+legend('approximate solution', 'exact solution');
+
+function [err, U, ele_coo] = One1DProblem(L, Ne, T, Nt, UL, UR)
+    dL = L / Ne;
+    ele_coo = (dL/2):dL:L;
+    
+    dt = T / Nt;
+
+    D = 1 / pi^2; Er = 0;
+    U_o = sin(pi * ele_coo) + ele_coo;
+
+    for i_tim = 1:Nt  % loop of time
+        MU = zeros(Ne); FU = zeros(Ne, 1);
+        for k = 1:Ne  % loop of volume
+            MU(k, k) = MU(k, k) + dL / dt;
+            FU(k) = FU(k) + dL / dt * U_o(k);
+            if k > 1 && k < Ne  % inside volume
+                MU(k, [k k-1]) = MU(k, [k k-1]) + D / dL * [1 -1];
+                MU(k, [k k+1]) = MU(k, [k k+1]) + D / dL * [1 -1];
+            end
+        end
+
+        % left boundary
+        MU(1, 1) = MU(1, 1) + D / dL * 2;
+        FU(1) = FU(1) + D / dL * 2 * UL;
+        MU(1, [1 2]) = MU(1, [1 2]) + D / dL * [1 -1];
+
+        % right boundary        
+        MU(Ne, [Ne Ne-1]) = MU(Ne, [Ne Ne-1]) + D / dL * [1 -1];
+        MU(Ne, Ne) = MU(Ne, Ne) + D / dL * 2;
+        FU(Ne) = FU(Ne) + D / dL * 2 * UR;
+
+        % solve system of linear equation
+        U = MU \ FU; 
+        U_o = U;
+
+        % compute error
+        Ert = 0; 
+        Ue = zeros(Ne, 1);
+        for k = 1:Ne
+            Ue(k) = exp(-i_tim * dt) * sin(pi * ele_coo(k)) + ele_coo(k);
+            Ert = Ert + (U(k) - Ue(k))^2 * dL;
+        end
+        Er = Er + dt * Ert;
+    end % loop of time
+    err = sqrt(Er); 
+    disp(err);
+end
